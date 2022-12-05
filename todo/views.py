d631f020efb04cc2a321b92e799ab547fba80434
brestok-1 <6234513@gmail.com>
2022-12-05 23:36:44 +0300

    second commit

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
 
-# Create your views here.
+
+# Create your views here
+def home(request):
+    return render(request, 'todo/home.html')
+
+
+def singupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/singupuser.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/singupuser.html',
+                              {'form': UserCreationForm(),
+                               'error': "That username has already been taken. Please, choose a new username"})
+        else:
+            return render(request, 'todo/singupuser.html',
+                          {'form': UserCreationForm(), 'error': "Password did't match"})
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if not user:
+            return render(request, 'todo/loginuser.html',
+                          {'form': AuthenticationForm(), 'error': "Username and password didn't match"})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -5,17 +5,26 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home.html, name='home.html')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home.html')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Auth
+    path('singup/', views.singupuser, name='singupuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+    path('login/', views.loginuser, name='loginuser'),
+    # Todo's
+    path('current/', views.currenttodos, name='currenttodos'),
+    path('', views.home, name='home'),
 ]
